b25c1ceb8924c5122f34e8d92a56f8a6e4c3cdb1
CamiDev <lamica.olivares@gmail.com>
2025-12-17 12:55:03 -0500

    redux auth

--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { uiSlice } from "./uiSlice";
 import { calendarSlice } from "./calendarSlice";
+import { authSlice } from "./authSlice";
 
 
 
 
 export const store = configureStore({
     reducer: {
+        auth:     authSlice.reducer,
         calendar: calendarSlice.reducer,
-        ui: uiSlice.reducer
+        ui:       uiSlice.reducer
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck: false
