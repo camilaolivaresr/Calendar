ecd37bd7ce2c1f90a4863fcddaaf3aa92ec296d9
CamiDev <lamica.olivares@gmail.com>
2025-12-30 14:08:38 -0500

    autenticacion error

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,6 +1,8 @@
+import { useEffect } from 'react'
 import useAuthStore from '../../hooks/useAuthStore'
 import { useForm } from '../../hooks/useForm'
 import './login.css'
+import Swal from 'sweetalert2'
 
 
 const loginFormFields = {
@@ -17,7 +19,7 @@ const registerFormFields = {
 
 const LoginPage = () => {
     
-    const {startLogin} = useAuthStore();
+    const {startLogin, errorMessage} = useAuthStore();
 
     const {loginEmail, loginPassword, onInputChange:onLoginInputChange} = useForm(loginFormFields);
     const {registerName, registerEmail, registerPassword, repeatRegisterPassword, onInputChange:onRegisterInputChange} = useForm(registerFormFields);
@@ -32,6 +34,12 @@ const LoginPage = () => {
         event.preventDefault();
         console.log({registerName, registerEmail, registerPassword, repeatRegisterPassword});
     }
+
+    useEffect(() => {
+        if(errorMessage !== undefined ) {
+            Swal.fire('Error de autenticacion', errorMessage, 'error')
+        }
+    }, [errorMessage])
 
   return (
         <div className="container login-container">
